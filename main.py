06f27dfcea603e958a1bfe18beb6f11375b04e8f
Théophile Garin <theophilegarin@hotmail.com>
2025-10-03 00:18:57 +0200

    feat(bot): Ajout de la gestiond d'erreur du bot

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ dotenv.load_dotenv()
 print("Lancement du bot ...")
 
 client = discord.Client(intents=discord.Intents.all())
-client.run(os.getenv('DISCORD_TOKEN'))
+try: 
+    client.run(os.getenv('DISCORD_TOKEN'))
+except discord.errors.LoginFailure:
+    (print("❌ Attention: Le token Discord est invalide ou manquant dans le .env"))
